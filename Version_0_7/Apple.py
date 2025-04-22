92b115c6e723a295bad9a0995bbfe6fb76a18327
UNSIJ <gutierrezbetetam95@gmail.com>
2025-04-22 16:05:02 -0600

    version 0.7

---
+++ Version_0_7/Apple.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import Sprite
+from Configurations import Configurations
+
+class Apple(Sprite):
+
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.Surface((10,10))
+        self.image.fill((25,0,0))
+
+        self.rect = self.image.get_rect()
+
+    def blit(self,screen: pygame.surface.Surface)->None:
+        """
+        Se utiliza para dibujar la manzana.
+        :param screen: Pantalla en donde se dibuja.
+        """
+        screen.blit(self.image,self.rect)
