10c4b3c1791261635fb92ff798063089b9610234
PERIFERICO <gutierrezbetetam)5@gmail.com>
2025-06-05 14:31:24 -0600

    6

---
+++ Soldiers_vs_aliens/Version_0_6/Game_functionalities.py
@@ -0,0 +1,87 @@
+import pygame
+from Configurations import Configurations
+from Media import Background
+from Soldier import Soldier
+"""NUEVO."""
+from Shot import Shot
+
+
+"""CAMBIO. Ahora recibe el grupo de los disparos del soldado."""
+def game_events(soldier: Soldier, gunshots: pygame.sprite.Group) -> bool:
+    """
+    Función que administra los eventos del juego.
+    :param soldier: Objeto con el soldado (personaje principal).
+    :param gunshots: Grupo que almacena los disparos del soldado.
+    :return: La bandera de fin del juego.
+    """
+    # Se declara la bandera de fin del juego que se retorna.
+    game_over = False
+
+    # Se verifican los eventos (teclado y ratón) del juego.
+    for event in pygame.event.get():
+        # El evento es un clic para cerrar el juego.
+        if event.type == pygame.QUIT:
+            game_over = True
+
+        # Se verifica el evento de presionar una tecla.
+        if event.type == pygame.KEYDOWN:
+            # Se verifica las flechas para el movimiento.
+            if event.key == pygame.K_UP:
+                soldier.is_moving_up = True
+
+            if event.key == pygame.K_DOWN:
+                soldier.is_moving_down = True
+
+            """NUEVO."""
+            # Si se presionó el espacio, entonces se crea un nuevo disparo y se agrega al grupo.
+            if event.key == pygame.K_SPACE:
+                new_shot = Shot(soldier)
+                gunshots.add(new_shot)
+
+        # Se verifica el evento de soltar una tecla.
+        if event.type == pygame.KEYUP:
+            # Se verifica las flechas para dejar de moverse.
+            if event.key == pygame.K_UP:
+                soldier.is_moving_up = False
+
+            if event.key == pygame.K_DOWN:
+                soldier.is_moving_down = False
+
+    # Se regresa la bandera.
+    return game_over
+
+
+"""CAMBIO. Ahora recibe el grupo de los disparos del soldado."""
+def screen_refresh(screen: pygame.surface.Surface,
+                   clock: pygame.time.Clock,
+                   background: Background,
+                   soldier: Soldier,
+                   gunshots: pygame.sprite.Group) -> None:
+    """
+    Función que administra los elementos de la pantalla.
+    :param screen: Objeto con la pantalla.
+    :param clock: Objeto con el reloj del videojuego.
+    :param background: Objeto con el fondo de pantalla.
+    :param soldier: Objeto con el soldado (personaje principal).
+    :param gunshots: Grupo que almacena los disparos del soldado.
+    """
+    # Se dibuja el fondo de la pantalla.
+    background.blit(screen)
+
+    # Se actualiza la posición del soldado, se anima su sprite y se dibuja en la pantalla.
+    soldier.update_position(screen)
+    soldier.update_animation()
+    soldier.blit(screen)
+
+    """NUEVO."""
+    # Se actualizan las posiciones, se animan y se dibujan los sprites del grupo de los disparos del soldado.
+    for shot in gunshots.sprites():
+        shot.update_position()
+        shot.update_animation()
+        shot.blit(screen)
+
+    # Se actualiza la pantalla, dando la impresión de movimiento.
+    pygame.display.flip()
+
+    # Se controla la velocidad de fotogramas (FPS) del videojuego.
+    clock.tick(Configurations.get_fps())
---
+++ Soldiers_vs_aliens/Version_0_6/Soldiers_vs_aliens.py
@@ -0,0 +1,60 @@
+"""
+Nombre: Alberto Martínez Barbosa.
+Fecha:
+Versión 0.4:
+
+"""
+
+# Se importan los módulos necesarios.
+import pygame
+from Configurations import Configurations
+from Game_functionalities import game_events, screen_refresh
+from Media import Background
+from Soldier import Soldier
+"""NUEVO."""
+from pygame.sprite import Group
+
+
+def run_game() -> None:
+    """
+    Función principal del videojuego.
+    """
+    # Se inicializa el módulo de pygame y se realizan las configuraciones iniciales.
+    pygame.init()
+    screen_size = Configurations.get_screen_size()
+    screen = pygame.display.set_mode(screen_size)
+    clock = pygame.time.Clock()  # Se usa para controlar la velocidad de fotogramas (FPS).
+
+    # Se configura el título de la ventana.
+    game_title = Configurations.get_game_title()
+    pygame.display.set_caption(game_title)
+
+    # Se crea el objeto del fondo de pantalla.
+    background = Background()
+
+    # Se crea el objeto del soldado (personaje principal).
+    soldier = Soldier(screen)
+
+    """NUEVO."""
+    # Se crea el grupo para almacenar los disparos del soldado.
+    gunshots = Group()
+
+    # Ciclo principal del videojuego.
+    game_over = False
+    while not game_over:
+        """CAMBIO. Ahora recibe el grupo de los disparos del soldado."""
+        # Función que administra los eventos del juego.
+        game_over = game_events(soldier, gunshots)
+
+        """CAMBIO. Ahora recibe el grupo de los disparos del soldado."""
+        # Función que administra los elementos de la pantalla.
+        screen_refresh(screen, clock, background, soldier, gunshots)
+
+    # Cierra todos los recursos del módulo pygame.
+    pygame.quit()
+
+
+
+""" %%%%%%%     CÓDIGO A NIVEL DE MÓDULO    %%%%%%%%%%%%%%%%%%%%% """
+if __name__ == "__main__":
+    run_game()
